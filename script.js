a29fb8780b9e8f11d3567f0262c9100764f1eaf2
perditioinc <perditio.dev@gmail.com>
2024-10-13 18:46:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-  e.preventDefault(); // Prevent page reload
-  alert('You will be notified soon!');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("notification-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent page reload
+    alert("You will be notified soon!");
+  });
 });
